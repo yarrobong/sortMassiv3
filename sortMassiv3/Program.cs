b94c0b64784a2466600d58fda845c268f3292b0f
yarrobong <oneboardsholi@gmail.com>
2023-12-12 22:45:04 +0500

    Добавьте файлы проекта.

---
+++ sortMassiv3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] A = { 3, -2, 4, 0, -6, 7, -1, 8, -5, 9 };
+
+        for (int i = 0; i < A.Length; i++)
+        {
+            if (A[i] < 0)
+            {
+                A[i] *= A[i]; 
+            }
+            else if (A[i] > 0)
+            {
+                A[i] /= 2; 
+            }
+        }
+
+        Array.Sort(A);
+
+        Console.WriteLine("Преобразованный и отсортированный массив:");
+        foreach (int element in A)
+        {
+            Console.Write(element + " ");
+        }
+    }
+}
